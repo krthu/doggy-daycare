7b8b6b76f3fd66ec57e882e26ef6805f3eee14d1
KT <thun.kristian@gmail.com>
2024-09-18 11:41:30 +0200

    Welcome scafolding

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+const Welcome = () =>{
+
+    return (
+        <div className="welcome-container">
+            <h3>Welcome to</h3>
+            <h2>Happy Tails Retreat!</h2>
+            
+        </div>
+
+    )
+}
+
+export default Welcome;
