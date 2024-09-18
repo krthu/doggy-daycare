74a8d82593c436d3e462d03d2ec0552ad8f90db6
KT <thun.kristian@gmail.com>
2024-09-18 20:08:02 +0200

    Navigate with chipID and som css for details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,20 +1,24 @@
 import { Link, useParams } from "react-router-dom";
 import './Details.css'
+import DetailsInfoItem from "./DetailsInfoItem";
 
 
 const Details = (props) => {
     if (!props.data) {
-        return <p>Loading dog...</p>; 
-      }
-      
-  
+        return <p>Loading dog...</p>;
+    }
+
+
     let list = props.data.record
-    
+
 
     const params = useParams();
-    const id = params.id;
-    const dogID = parseInt(id);
-    const dog = dogID >= 0 && dogID < list.length ? list[dogID] : null;
+    // const id = params.id;
+    // const dogID = parseInt(id);
+    // const dog = dogID >= 0 && dogID < list.length ? list[dogID] : null;
+
+    const chipNumber = params.chipNumber;
+    const dog = list.find(dog => dog.chipNumber === chipNumber)
 
     if (!dog) {
         return <p>No dog found!</p>
@@ -23,23 +27,25 @@ const Details = (props) => {
 
     return (
         <div className="details-container">
-            <Link to={'/catalog'}> 
-                <span class="material-symbols-outlined">
+            <Link to={'/catalog'} className="back-link">
+                <span class="material-symbols-outlined back-arrow">
                     arrow_back_ios
                 </span>
             </Link>
-            <img src={dog.img} alt={`${dog.name} is a ${dog.breed}`} className="large-profile-image" />
+            <img src={dog.img} alt={`${dog.name} is a ${dog.breed}`} className={`large-profile-image ${dog.present ? 'details-present' : ''}`   }/>
             <section className="information-section">
-                <h3 className="name">Name: {dog.name}</h3>
-                <p>Sex: {dog.sex}</p>
-                <p>Breed: {dog.breed}</p>
-                <p>Age: {dog.age}</p>
-                <p>ChipNumber: {dog.chipNumber}</p>
-                <p>Owner: {dog.owner.name} {dog.owner.lastName}</p>
-                <p>Phone Number: {dog.owner.phoneNumber}</p>
+                <h3 className="name">{dog.name} <span class="material-symbols-outlined gender-span">
+                {dog.sex} </span></h3>
+                {/* <section className="details-info-section"> */}
+                    <DetailsInfoItem label={'Breed'} value={dog.breed}/>
+                    <DetailsInfoItem label={'Age'} value={dog.age} />
+                    <DetailsInfoItem label={'Chip number'} value={dog.chipNumber} />
+                    <DetailsInfoItem label={'Owner'} value={`${dog.owner.name} ${dog.owner.lastName}`} />
+                    <DetailsInfoItem label={'Phone'} value={dog.owner.phoneNumber} />
+                {/* </section> */}
             </section>
         </div>
-     
+
     )
 }
 
