98b8f4214c9433ff29c48381ca9df9874f8261a7
KT <thun.kristian@gmail.com>
2024-09-19 11:08:52 +0200

    Added filter for sex and breed

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 import ListItem from "./ListItem";
 import List from "./List";
 import "./Catalog.css";
+import DropDownInput from "./DropdownInput";
 
 
 const Catalog = (props) => {
     const [searchInput, setSearchInput] = useState('');
+    const [isFilterMenuOpen, setIsFilterMenuOpen] = useState(false)
+    const [selectedBreed, setSelectedBreed] = useState('')
+    const [selectedSex, setSelectedSex] = useState('')
 
     const sortAlphabetically = (a, b) => {
         if (a.name < b.name) {
@@ -27,11 +31,27 @@ const Catalog = (props) => {
     const list = props.data.record.sort(sortAlphabetically);
 
     const filteredList = list.filter(dog =>
-        dog.name.toLowerCase().includes(searchInput.toLowerCase())
+        dog.name.toLowerCase().includes(searchInput.toLowerCase()) &&
+        (dog.breed.toLowerCase() === (selectedBreed.toLowerCase()) || selectedBreed === '') &&
+        (dog.sex.toLowerCase() === (selectedSex.toLowerCase()) || selectedSex === '')
       );
     const handleInputChange = (e) => {
         setSearchInput(e.target.value);
     }
+
+    const handleChangeBreed = (e) => {
+        setSelectedBreed(e.target.value)
+        // setIsFilterMenuOpen(false)
+    }
+
+    const handleChangeSex = (e) => {
+        setSelectedSex(e.target.value)
+    }
+
+
+
+    const uniqueBreeds = new Set(list.map(dog => dog.breed));
+    console.log(selectedBreed);
 
 
     return (
@@ -40,12 +60,43 @@ const Catalog = (props) => {
                 <input placeholder="Search"
                     value={searchInput}
                     onChange={handleInputChange} />
-                <span className="material-symbols-outlined">
+                <span className="material-symbols-outlined" onClick={() => setIsFilterMenuOpen(!isFilterMenuOpen)}>
                     filter_list
                 </span>
             </section>
 
+            {isFilterMenuOpen && (
+                <div className="filter-overlay">
+                    <section className="filter-section">
+                        <h2>Filter</h2>
+                        <span className="material-symbols-outlined close-filter" onClick={() => setIsFilterMenuOpen(false)}>
+                        close
+                        </span>
+                        <section className="filter-option">
+                            <label>Breed</label>
+                           <DropDownInput 
+                                selectedOption={selectedBreed} 
+                                handleOptionChange={handleChangeBreed} 
+                                options={Array.from(uniqueBreeds)}
+                                optionName={'Breed'}
+                                />
+                        </section>
+                        <section className="filter-option">
+                            <label>Sex</label>
+                            <DropDownInput
+                                selectedOption={selectedSex} 
+                                handleOptionChange={handleChangeSex}
+                                options={['male', 'female']}
+                                optionName={'Sex'}
+                                />
+                       </section>
+
+                    </section>
+                </div>
+            )}
+
             <List list={filteredList} />
+
         </>
 
     )
