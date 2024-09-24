d2289a106788ba6a0601f2ec6d7fbd0950ea65de
KT <thun.kristian@gmail.com>
2024-09-24 15:58:25 +0200

    First version of landing page done

---
+++ src/components/LargeButton.jsx
@@ -0,0 +1,18 @@
+const LargeButton = (props) => {
+
+
+    return(
+        <div className={`large-button ${props.class}`}>
+        <div className="large-button-text-container">
+            <span className="material-symbols-outlined large-button-icon">
+                {props.icon}
+            </span>
+            <p className="large-button-text">{props.text}</p>
+        </div>
+
+    </div>
+    )
+
+}
+
+export default LargeButton;
